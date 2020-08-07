0297a4ebd44224709faad123ced839a9d75f5fca
hengyunabc <hengyunabc@gmail.com>
2020-08-07 17:46:29 +0800

    add feature desc

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -27,35 +27,35 @@ export default {
       title: '特性一览',
       list: [
         {
-          img: '/img/feature_transpart.png',
-          title: '特性1',
-          content: '特性1的简单概括',
+          img: '/img/feature_hogh.png',
+          title: 'Dashboard',
+          content: '实时查看系统的运行状况',
         },
         {
           img: '/img/feature_loadbalances.png',
-          title: '特性2',
-          content: '特性2的简单概括',
+          title: '查看入参/返回值/异常',
+          content: '查看函数调用的参数，返回值和异常',
+        },
+        {
+          img: '/img/feature_transpart.png',
+          title: '在线热更新',
+          content: 'jad/sc/redefine 一条龙热更新代码',
         },
         {
           img: '/img/feature_service.png',
-          title: '特性3',
-          content: '特性3的简单概括',
-        },
-        {
-          img: '/img/feature_hogh.png',
-          title: '特性4',
-          content: '特性4的简单概括',
+          title: '类冲突',
+          content: '秒解类冲突问题，定位类加载路径',
         },
         {
           img: '/img/feature_runtime.png',
-          title: '特性5',
-          content: '特性5的简单概括',
+          title: '性能热点',
+          content: '快速定位应用的热点，生成火焰图',
         },
         {
           img: '/img/feature_maintenance.png',
-          title: '特性6',
-          content: '特性6的简单概括',
-        },
+          title: 'WebConsole',
+          content: '在线诊断，点开网页诊断线上应用',
+        }
       ],
     },
 
@@ -92,42 +92,42 @@ export default {
       ],
     },
     introduction: {
-      title: 'introduction title',
-      desc: 'some introduction of your product',
+      title: 'Java Diagnostic Tool',
+      desc: 'Arthas is a Java diagnostic tool open-sourced by Alibaba middleware team. It is widely adopted and popular among the developers.',
       img: '/img/dashboard.png',
     },
     features: {
       title: 'Feature List',
       list: [
         {
-          img: '/img/feature_transpart.png',
-          title: 'feature1',
-          content: 'feature description',
+          img: '/img/feature_hogh.png',
+          title: 'Dashboard',
+          content: 'View the operating status of the system in real time',
         },
         {
           img: '/img/feature_loadbalances.png',
-          title: 'feature2',
-          content: 'feature description',
+          title: 'Parameters/Return values/Exceptions',
+          content: 'View method parameters, return values and exceptions',
+        },
+        {
+          img: '/img/feature_transpart.png',
+          title: 'Online hotswap',
+          content: 'jad/sc/redefine online hotswap',
         },
         {
           img: '/img/feature_service.png',
-          title: 'feature3',
-          content: 'feature description',
-        },
-        {
-          img: '/img/feature_hogh.png',
-          title: 'feature4',
-          content: 'feature description',
+          title: 'Class conflict',
+          content: 'Resolve the class conflict problem in seconds, locate the class loading path',
         },
         {
           img: '/img/feature_runtime.png',
-          title: 'feature5',
-          content: 'feature description',
+          title: 'Flame Graph',
+          content: 'Quickly locate application hotspots and generate flame graphs',
         },
         {
           img: '/img/feature_maintenance.png',
-          title: 'feature6',
-          content: 'feature description',
+          title: 'WebConsole',
+          content: 'Online diagnosis',
         }
       ]
     },
